a0bee8d455005788ce56548b395d6b30a40204cc
Manuel Lopez <74105210+ManoloTonto1@users.noreply.github.com>
2023-02-24 13:05:44 +0100

    Donn!!!

--- Infected/game/game.go
+++ Infected/game/game.go
@@ -24,10 +24,17 @@ func CreateEmptyBoard(size int) Board {
 	}
 	// set the players in the correct location
 	spaces[size-1][0] = Player1
+	spaces[size-1][1] = Player1
+	spaces[size-2][0] = Player1
+	spaces[size-2][1] = Player1
+
+	spaces[0][size-2] = Player2
+	spaces[1][size-2] = Player2
 	spaces[0][size-1] = Player2
+	spaces[1][size-1] = Player2
 	players := []Player{
-		{ID: Player1, Infected: 1},
-		{ID: Player2, Infected: 1},
+		{ID: Player1, Infected: 4},
+		{ID: Player2, Infected: 4},
 	}
 
 	board := Board{Size: size, Spaces: spaces, Players: players}
@@ -43,24 +50,9 @@ func (game *Game) ServeServer() {
 }
 
 func (g *Game) ConsoleGame(Board Board) {
-	for i, arr := range Board.Spaces {
-		for j := range arr {
-			fmt.Print(Board.Spaces[i][j])
-		}
-		fmt.Printf("\n")
-	}
+	Board.PrintBoard()
+	CheckGameFinished(Board, g)
 
-	if Board.Players[0].Infected == 0 || Board.Players[1].Infected == 0 {
-		g.GameEnded = true
-	}
-	if g.GameEnded {
-		fmt.Println("Game ended")
-		fmt.Println("winning Player infected: ", Board.Players[1].Infected)
-		fmt.Println("Turns: ", g.Turns)
-		fmt.Scanln()
-		g.Start()
-		return
-	}
 	fmt.Println("Player 1 turn")
 	player1Move := Board.CalculateBestMoveSet(Player1, Player2)
 	if err := Board.Move(Player1, player1Move); err != nil {
@@ -68,12 +60,9 @@ func (g *Game) ConsoleGame(Board Board) {
 	}
 
 	fmt.Println("------------------")
-	for i, arr := range Board.Spaces {
-		for j := range arr {
-			fmt.Print(Board.Spaces[i][j])
-		}
-		fmt.Printf("\n")
-	}
+	Board.PrintBoard()
+	CheckGameFinished(Board, g)
+
 	fmt.Println("Player 2 turn")
 
 	aiMove := Board.CalculateBestMoveSet(Player2, Player1)
@@ -86,10 +75,88 @@ func (g *Game) ConsoleGame(Board Board) {
 	g.ConsoleGame(Board)
 
 }
+
+func CheckGameFinished(Board Board, g *Game) {
+	if Board.Players[0].Infected == 0 || Board.Players[1].Infected == 0 {
+		g.GameEnded = true
+	}
+	if g.GameEnded {
+		fmt.Println("Game ended")
+		fmt.Println("winning Player infected: ", Board.Players[1].Infected)
+		fmt.Println("Turns: ", g.Turns)
+		fmt.Scanln()
+		g.Start()
+	}
+}
+
+func (Board Board) PrintBoard() {
+	print("-|")
+	for i := 0; i < Board.Size; i++ {
+		fmt.Print(i)
+	}
+	fmt.Printf("\n")
+	for i := 0; i < Board.Size+2; i++ {
+
+		fmt.Print("-")
+	}
+	fmt.Printf("\n")
+	for i, arr := range Board.Spaces {
+		fmt.Print(i)
+		fmt.Print("|")
+		for j := range arr {
+			fmt.Print(Board.Spaces[i][j])
+		}
+		fmt.Printf("\n")
+	}
+}
+func (g *Game) PlayerGame(Board Board) {
+
+	Board.PrintBoard()
+	CheckGameFinished(Board, g)
+
+	fmt.Println("Player 1 turn")
+	fmt.Println("Enter the coordinates of the piece you want to move (y,x)")
+	var x, y int
+	fmt.Scanln(&x, &y)
+	fmt.Println("Enter the coordinates of the space you want to move to (y,x)")
+	var x2, y2 int
+	fmt.Scanln(&x2, &y2)
+	player1Move := Move{From: Coordinates{X: x, Y: y}, To: Coordinates{X: x2, Y: y2}}
+	if err := Board.Move(Player1, player1Move); err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("------------------")
+	Board.PrintBoard()
+	CheckGameFinished(Board, g)
+
+	fmt.Println("Player 2 turn")
+
+	aiMove := Board.CalculateBestMoveSet(Player2, Player1)
+
+	if err := Board.Move(Player2, aiMove); err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("------------------")
+	g.Turns++
+	g.PlayerGame(Board)
+}
+
 func (g *Game) Start() {
 	g.Turns = 0
 	g.GameEnded = false
 	Board := CreateEmptyBoard(9)
-
-	g.ConsoleGame(Board)
+	fmt.Println("------------------")
+	fmt.Println("Choose a game mode:")
+	fmt.Println("1. Console")
+	fmt.Println("2. P vs M")
+	fmt.Println("------------------")
+	var gameMode int
+	fmt.Scanln(&gameMode)
+	if gameMode == 1 {
+		g.ConsoleGame(Board)
+	}
+	if gameMode == 2 {
+		g.PlayerGame(Board)
+	}
 }
